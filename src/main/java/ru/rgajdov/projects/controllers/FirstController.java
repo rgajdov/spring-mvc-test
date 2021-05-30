12f3a3c7f7f0fe9bc28edfc417f936afe3926ed9
r.gaidov <r.gaidov@seraphim.online>
2021-05-30 17:47:16 +0300

    Add request parameters

--- src/main/java/ru/rgajdov/projects/controllers/FirstController.java
+++ src/main/java/ru/rgajdov/projects/controllers/FirstController.java
@@ -2,6 +2,7 @@ package ru.rgajdov.projects.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
