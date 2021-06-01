b8d0243347fb2e9982d563143a8d71b503a6fad1
r.gaidov <r.gaidov@seraphim.online>
2021-06-01 14:33:13 +0300

    Add creating people operation

--- src/main/java/ru/rgajdov/projects/controllers/PeopleController.java
+++ src/main/java/ru/rgajdov/projects/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.rgajdov.projects.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.rgajdov.projects.dao.PersonDAO;
+import ru.rgajdov.projects.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -31,5 +30,26 @@ public class PeopleController {
         // Получим одного человека по id из DAO и передадим на отображение в представление
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    /*
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        // Отображаем форму для добавления нового people
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+    */
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        // Отображаем форму для добавления нового people
+        //model.addAttribute("person", personDAO.show(id));
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
